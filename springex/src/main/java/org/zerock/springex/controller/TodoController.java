c97c108471edb69c90aafaa8c63aaf9a804c1f5f
Park Sungjun <34852381a@gmail.com>
2022-12-06 18:03:53 +0900

    22-12-06 commit spring Web MVC study

--- springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,9 +2,11 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springex.dto.TodoDTO;
 
 @Controller
@@ -13,7 +15,7 @@ import org.zerock.springex.dto.TodoDTO;
 public class TodoController {
 
     @RequestMapping("/list")
-    public void list() {
+    public void list(Model model) {
         log.info("todo list..........");
     }
 
@@ -22,10 +24,13 @@ public class TodoController {
         log.info("GET todo register........");
     }
 
-    @PostMapping("/register")
-    public void registerPost(TodoDTO todoDTO) {
-        log.info("POST todo register.......");
+@PostMapping("/register")
+    public String registerPost(TodoDTO todoDTO, RedirectAttributes redirectAttributes) {
+        log.info("POST todo register...........");
+
         log.info(todoDTO);
+
+        return "redirect:/todo/list";
     }
 
 }
