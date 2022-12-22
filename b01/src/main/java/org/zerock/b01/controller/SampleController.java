b02055868db1773ef1544f206e7730879e713749
Park Sungjun <34852381a@gmail.com>
2022-12-22 16:54:40 +0900

    sprint boot study thymeleaf layout

--- b01/src/main/java/org/zerock/b01/controller/SampleController.java
+++ b01/src/main/java/org/zerock/b01/controller/SampleController.java
@@ -77,4 +77,11 @@ public class SampleController {
 
     }
 
+    @GetMapping("/ex/ex3")
+    public void ex3(Model model) {
+
+        model.addAttribute("arr", new String[]{"AAA", "BBB", "CCC"});
+
+    }
+
 }
