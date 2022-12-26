a4c3bdc58027b07a4b692dae2eab7b5bd05d13b1
Park Sungjun <34852381a@gmail.com>
2022-12-26 17:24:14 +0900

    spring boot spring Data JPA study

---
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,28 @@
+package org.zerock.b01.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false) // 칼럼의 길이와 null 허용 여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+
+}
